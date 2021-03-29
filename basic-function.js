6895bbdf7a97f52d28818398c51be79c48f2b2c8
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-03-29 12:48:30 -0500

    added basic function page and updated style sheet

---
+++ basic-function.js
@@ -0,0 +1,6 @@
+var message = 'Sign up for my newsletter and receive 10% off!';
+function updateMessage() {
+    var el = document.getElementById('message');
+    el.textContent = msg;
+}
+updateMessage();
